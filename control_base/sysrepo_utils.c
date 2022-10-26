c19e9c11ef663c0f84a121f0f6ea41bb613dc626
wldyd423 <wldyd423@github.com>
2022-10-26 15:19:27 +0900

    debug

--- control_base/sysrepo_utils.c
+++ control_base/sysrepo_utils.c
@@ -347,7 +347,7 @@ int readIPSEC_conn_entry(sr_session_ctx_t *sess, sr_change_iter_t *it, char *xpa
 
 
         if (0 == strcmp("/action", name)) {
-            DBG("action = %s", value->data.string_val);
+            DBG("action = %s, xpath = %s", value->data.string_val, value->xpath);
             if (!strcasecmp(value->data.string_val, "protect"))
                 action_policy_type=IPSEC_POLICY_PROTECT;
             else if (!strcasecmp(value->data.string_val, "bypass"))
